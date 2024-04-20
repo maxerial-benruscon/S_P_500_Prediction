ccb50e3f5715db4c15639caa4b96424dec28b16f
maxerial-benruscon <benito.rusconi@maxerial.io>
2024-04-20 16:32:02 +0200

    added more plots

--- ploting.py
+++ ploting.py
@@ -13,5 +13,17 @@ def plot_data(data_train, data_valid, data_test, y_train_pred, y_valid_pred, y_t
     fig.add_trace(go.Scatter(x=data_valid.loc[data_valid['Symbol'] == symbol].index, y=y_valid_pred[data_valid['Symbol'] == symbol], name='Validation prediction'))
     fig.add_trace(go.Scatter(x=data_test.loc[data_test['Symbol'] == symbol].index, y=y_test_pred[data_test['Symbol'] == symbol], name='Test prediction'))
 
-    fig.update_layout(title='AAPL Close price', legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+    fig.update_layout(title=f'{symbol} Close price', legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
     fig.write_html(f'plots/{model}_prediction_{symbol}.html')
+    
+    #create boxplot diffrence between ground truth and prediction
+    fig = go.Figure()
+    fig.add_trace(go.Box(y=y_train_pred[data_train['Symbol'] == symbol]-data_train.loc[data_train['Symbol'] == symbol]['Close'], name='Train'))
+    fig.add_trace(go.Box(y=y_valid_pred[data_valid['Symbol'] == symbol]-data_valid.loc[data_valid['Symbol'] == symbol]['Close'], name='Validation'))
+    fig.add_trace(go.Box(y=y_test_pred[data_test['Symbol'] == symbol]-data_test.loc[data_test['Symbol'] == symbol]['Close'], name='Test'))
+    
+    fig.update_layout(title=f'{symbol} Difference between prediction and ground truth', legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+    fig.write_html(f'plots/{model}_difference_{symbol}.html')
+    
+    
+    
