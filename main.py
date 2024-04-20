de274382e9ebcb16f0ebee43175310704b5013da
maxerial-benruscon <benito.rusconi@maxerial.io>
2024-04-20 15:52:44 +0200

    added RNN

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data_generator import download_stock_df, rsi_calculation
 from pipeline import pipeline
-from models import model_Ridge
+from models import model_LinearRegression, RNNModel
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 if __name__ == '__main__':
@@ -11,18 +11,26 @@ if __name__ == '__main__':
     
     X_train, y_train, X_valid, y_valid, X_test, y_test = pipeline(df)
     
-    model_Ridge.fit(X_train, y_train)
+    model_LinearRegression.fit(X_train, y_train)
     
-    print("Ridge regression model score on training set: ", model_Ridge.score(X_train, y_train))
-    print("Ridge regression model score on validation set: ", model_Ridge.score(X_valid, y_valid))
-    print("Ridge regression model score on test set: ", model_Ridge.score(X_test, y_test))
+    
     
     #mae
-    print("Mean absolute error on training set: ", mean_absolute_error(y_train, model_Ridge.predict(X_train)))
-    print("Mean absolute error on validation set: ", mean_absolute_error(y_valid, model_Ridge.predict(X_valid)))
-    print("Mean absolute error on test set: ", mean_absolute_error(y_test, model_Ridge.predict(X_test)))
+    print("Mean absolute error on training set: ", mean_absolute_error(y_train, model_LinearRegression.predict(X_train)))
+    print("Mean absolute error on validation set: ", mean_absolute_error(y_valid, model_LinearRegression.predict(X_valid)))
+    print("Mean absolute error on test set: ", mean_absolute_error(y_test, model_LinearRegression.predict(X_test)))
     
+    RNNModel = RNNModel(X_train.shape[1])
     
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+    X_valid = X_valid.reshape(X_valid.shape[0], X_valid.shape[1], 1)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+    
+    RNNModel.model.fit(X_train, y_train, epochs=25, batch_size=32, validation_data=(X_valid, y_valid))
+    
+    print("Mean absolute error on training set: ", mean_absolute_error(y_train, RNNModel.model.predict(X_train)))
+    print("Mean absolute error on validation set: ", mean_absolute_error(y_valid, RNNModel.model.predict(X_valid)))
+    print("Mean absolute error on test set: ", mean_absolute_error(y_test, RNNModel.model.predict(X_test)))
 
     
     
--- models.py
+++ models.py
@@ -1,4 +1,30 @@
 #ridge regression model
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Flatten, Dropout
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error
 
-model_Ridge = Ridge()
+#Baseline Model
+model_LinearRegression = LinearRegression()
+
+class RNNModel:
+    def __init__(self, shape):
+        self.shape = shape
+        self.model = self.build_model()
+
+    def build_model(self):
+        model = Sequential()
+        model.add(LSTM(256, activation='tanh', input_shape=(self.shape, 1), return_sequences=True))
+        model.add(Dropout(0.5))
+        model.add(LSTM(256, activation='tanh', return_sequences=True))
+        model.add(Dropout(0.5))
+        model.add(LSTM(128, activation='tanh'))
+        model.add(Dropout(0.5))
+        model.add(Dense(60, activation='tanh'))
+        model.add(Dense(1))
+
+        model.compile(optimizer='adam', loss='mae', metrics=['mae'])
+        return model
+
+
