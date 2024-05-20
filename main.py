4ac3121477c3b7242a23af4a1fce950834c43eab
maxerial-benruscon <benito.rusconi@maxerial.io>
2024-05-20 10:08:57 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,133 +1,46 @@
-from data_generator import download_stock_df
-from pipeline import pipeline
-from models import model_LinearRegression, model_RandomForest, model_XGB, RNNModel
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+import os
+from feature_generation import prepare_features, combine_stocks, ml_preprocessing
+from models import model_LinearRegression
 from ploting import plot_data
-import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
 
-if __name__ == '__main__':
-    
-    tech_stocks = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA', 'NVDA', 'INTC', 'AMD', 'QCOM', 'IBM']
-    df = download_stock_df(tech_stocks)
+# Aktien auswählen
+relevant_stocks = ['MSFT', 'AAPL', 'NVDA', 'GOOG', 'AMZN', 'BRK-B', 'LLY', 'JPM', 'XOM', 'WMT', 'UNH', 'MA', 'PG', 'JNJ', 'COST', 'HD', 'MRK', 'ORCL', 'CVX', 'BAC', 'KO', 'CRM', 'NFLX', 'PEP', 'AMD', 'TMO', 'ADBE', 'WFC', 'LIN', 'QCOM', 'CSCO', 'MCD', 'ACN', 'DIS', 'DHR', 'ABT', 'INTU', 'GE', 'CAT', 'AMAT', 'AXP', 'TXN', 'VZ', 'AMGN', 'PFE', 'MS', 'CMCSA', 'IBM', 'NEE', 'UNP']
 
-    
-    X_train, y_train, X_valid, y_valid, X_test, y_test, df_train, df_valid, df_test = pipeline(df)
-    
-    #######################################################################################
-    ### BaseLine Model Linear Regression
-    #######################################################################################
-    
-    model_LinearRegression.fit(X_train, y_train)
-    
-    y_pred_train = model_LinearRegression.predict(X_train)
-    y_pred_valid = model_LinearRegression.predict(X_valid)
-    y_pred_test = model_LinearRegression.predict(X_test)
-    
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AAPL', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='GOOGL', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='MSFT', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMZN', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='TSLA', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='NVDA', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='INTC', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMD', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='QCOM', model='Linear Regression')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='IBM', model='Linear Regression')
-    
-    #mae
-    print("Linear Regression ----------------------------------------------------")
-    print("Mean absolute error on training set: ", mean_absolute_error(y_train, y_pred_train))
-    print("Mean absolute error on validation set: ", mean_absolute_error(y_valid, y_pred_valid))
-    print("Mean absolute error on test set: ", mean_absolute_error(y_test, y_pred_test))
-    
+# Feature-Generation für jede Aktie
+for symbol in relevant_stocks:
+    file_path = os.path.join('data', 'stock_dataframes', f'{symbol}.csv')
+    if not os.path.exists(file_path): # nicht nochmals erstellen wenn schon vorhanden
+        prepare_features(symbol, option_volume=False)
 
-    
-    
-    ################################################################################################
-    ### BaseLine Model Random Forest
-    ################################################################################################
-    
-    model_RandomForest.fit(X_train, y_train)
-    
-    y_pred_train = model_RandomForest.predict(X_train)
-    y_pred_valid = model_RandomForest.predict(X_valid)
-    y_pred_test = model_RandomForest.predict(X_test)
-    
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AAPL', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='GOOGL', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='MSFT', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMZN', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='TSLA', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='NVDA', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='INTC', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMD', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='QCOM', model='Random Forest')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='IBM', model='Random Forest')
-    
-    #mae
-    print("Random Forrest ----------------------------------------------------")
-    print("Mean absolute error on training set: ", mean_absolute_error(y_train, y_pred_train))
-    print("Mean absolute error on validation set: ", mean_absolute_error(y_valid, y_pred_valid))
-    print("Mean absolute error on test set: ", mean_absolute_error(y_test, y_pred_test))
-    
-    
-    ################################################################################################
-    ### BaseLine Model XGB
-    ################################################################################################
-    
-    
-    model_XGB.fit(X_train, y_train)
-    
-    y_pred_train = model_XGB.predict(X_train)
-    y_pred_valid = model_XGB.predict(X_valid)
-    y_pred_test = model_XGB.predict(X_test)
-    
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AAPL', model='XGB')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='GOOGL', model='XGB')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='MSFT', model='XGB')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMZN', model='XGB')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='TSLA', model='XGB')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='NVDA', model='XGB')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='INTC', model='XGB')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMD', model='XGB')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='QCOM', model='XGB')
-    
-    #mae
-    print("XGB ----------------------------------------------------")
-    print("Mean absolute error on training set: ", mean_absolute_error(y_train, y_pred_train))
-    print("Mean absolute error on validation set: ", mean_absolute_error(y_valid, y_pred_valid))
-    print("Mean absolute error on test set: ", mean_absolute_error(y_test, y_pred_test))
-    
-    
-    
-    ################################################################################################
-    ### RNN Model
-    ################################################################################################
-    
-    RNNModel = RNNModel(X_train.shape[1])
-    
-    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-    X_valid = X_valid.reshape(X_valid.shape[0], X_valid.shape[1], 1)
-    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
-    
-    history = RNNModel.model.fit(X_train, y_train, epochs=1, batch_size=8, validation_data=(X_valid, y_valid))
-    
-    y_pred_train = RNNModel.model.predict(X_train)
-    y_pred_valid = RNNModel.model.predict(X_valid)
-    y_pred_test = RNNModel.model.predict(X_test)
-    
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AAPL', model='RNN')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='GOOGL', model='RNN')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='MSFT', model='RNN')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMZN', model='RNN')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='TSLA', model='RNN')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='NVDA', model='RNN')
-    plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='INTC', model='RNN')
-    
-    
-    
-    print("RNN ----------------------------------------------------")
-    print("Mean absolute error on training set: ", mean_absolute_error(y_train, y_pred_train))
-    print("Mean absolute error on validation set: ", mean_absolute_error(y_valid, y_pred_valid))
-    print("Mean absolute error on test set: ", mean_absolute_error(y_test, y_pred_test))
-    
+# Zusammenführen, Clean-up NaN-Values (im Normalfall 1 Zeile), One-Hot Encoding der kategorischen Variable 'Sector', abspeichern 
+stock_df = combine_stocks(relevant_stocks, return_df=True)
+
+# Vorbereitung des Trainings-, Validierungs- und Testdatensatzes
+X_train_scaled, y_train, X_valid_scaled, y_valid, X_test_scaled, y_test, df_train, df_valid, df_test = ml_preprocessing(stock_df)
+
+print("Training...")
+model_LinearRegression.fit(X_train_scaled, y_train)
+
+y_pred_train = model_LinearRegression.predict(X_train_scaled)
+y_pred_valid = model_LinearRegression.predict(X_valid_scaled)
+y_pred_test = model_LinearRegression.predict(X_test_scaled)
+
+###Results###
+print("Linear Regression ----------------------------------------------------")
+print("Mean absolute error on training set: ", mean_absolute_error(y_train, y_pred_train))
+print("Mean absolute error on validation set: ", mean_absolute_error(y_valid, y_pred_valid))
+print("Mean absolute error on test set: ", mean_absolute_error(y_test, y_pred_test))
+
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AAPL', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='GOOGL', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='MSFT', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMZN', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='TSLA', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='NVDA', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='INTC', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='AMD', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='QCOM', model='Linear Regression')
+plot_data(df_train, df_valid, df_test, y_pred_train, y_pred_valid, y_pred_test, symbol='IBM', model='Linear Regression')
+print("Done")
+#mae
